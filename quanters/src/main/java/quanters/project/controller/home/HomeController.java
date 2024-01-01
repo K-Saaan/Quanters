d6130cb16a83d543286697c6c2d07f93b596aed8
Meocintosh <kimdh7776@naver.com>
2024-01-01 17:14:27 +0900

    feat : 상세화면 로직 추가 개발
    홈페이지 화면에서 검색어 입력했을시 이동되는 상세화면 로직 추가 개발

--- quanters/src/main/java/quanters/project/controller/home/HomeController.java
+++ quanters/src/main/java/quanters/project/controller/home/HomeController.java
@@ -2,8 +2,12 @@ package quanters.project.controller.home;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 
 @Controller // ResponseBody 필요없음
 @RequiredArgsConstructor // final 객체를 Constructor Injection 해줌. Autowired 필요없음
@@ -12,5 +16,19 @@ public class HomeController {
     @GetMapping("/search")
     public String showTestPage() {
         return "home/search";
+    }
+
+    @GetMapping("/detail")
+    public String searchDetail(@RequestParam(value = "keyword") String keyword, Model model) {
+        model.addAttribute("keyword", keyword);
+        try {
+            CsvParser csvParser = new CsvParser();
+            csvParser.csvParser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "home/detail";
     }
 }
---
+++ quanters/src/main/webapp/WEB-INF/views/home/detail.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<script type="text/javascript" src="${pageContext.request.contextPath}/static/js/home/detail.js"></script>
+<div class="d-flex justify-content-center">
+    <p>Keyword : ${keyword}</p>
+</div>
