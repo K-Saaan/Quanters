0a8f6f94d11571272cc02f50c144426f5aa3b498
Meocintosh <kimdh7776@naver.com>
2024-02-05 21:48:35 +0900

    build : from jar to war 빌드 방식 변경
    서버에서 webapp/WEB-INF 경로를 사용 가능하게 하기 위해 jar 방식에서 war 방식으로 빌드 방식 변경

--- quanters/src/main/java/quanters/project/QuantersAdminApplication.java
+++ quanters/src/main/java/quanters/project/QuantersAdminApplication.java
@@ -2,10 +2,17 @@ package quanters.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QuantersAdminApplication {
+public class QuantersAdminApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(QuantersAdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(QuantersAdminApplication.class);
     }
 }
