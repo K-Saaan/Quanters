ae8e3984df4bda7dde39966d8456a41e8b50ace0
Meocintosh <kimdh7776@naver.com>
2024-05-03 17:17:00 +0900

    feat : 로그인 시 파라미터 추가

--- quanters/src/main/java/quanters/project/service/login/impl/LoginServiceImpl.java
+++ quanters/src/main/java/quanters/project/service/login/impl/LoginServiceImpl.java
@@ -37,8 +37,49 @@ public class LoginServiceImpl implements LoginService {
         logger.info("user :: " + user);
         if(user != null) {
             logger.info("login success >>>>>>>");
+
+            // 메인으로 이동
+            resultMap.put("RESULT", "GO_MAIN");
+//            resultMap.put("URL", user.getMainUrl());
+
+            // 로그인 시간 update / 로그인 실패 횟수 0으로 초기화
+//            mng.LoginUpdate(0, "N", id, today);
+//            mngRepository.save(mng);
+//
+//            String stringToday = DateUtil.getStringToday();
+//
+//            // 로그인 이력 기록
+//            LoginHistEntity loginHistEntity = LoginHistEntity.builder().mngId(id).loginDate(stringToday).regId(mng.getRegId())
+//                    .regDate(mng.getRegDate()).chgrId(mng.getChgrId()).chgrDate(dateTime).build();
+
+//            loginHsitRepository.save(loginHistEntity);
+
         }else {
             logger.info("login fail >>>>>>>");
+            resultMap.put("RESULT", "LOGIN_FAIL");
+            resultMap.put("URL", "");
+//            MngEntity idCheck = mngRepository.findByMngId(id);
+//            String dateTime = LocalDateTime.now().toString();
+//            if(idCheck != null) {
+//                int failCnt = idCheck.getLoginFailCnt();
+//
+//                if(failCnt >= 5) {
+//                    resultMap.put("RESULT", "OVER_LOGIN_FAIL_CNT");
+//                    resultMap.put("URL", "");
+//                    idCheck.LoginUpdate(failCnt+1, "Y", id, dateTime);
+//                    mngRepository.save(idCheck);
+//                }else {
+//                    resultMap.put("RESULT", "PWD_FAIL");
+//                    resultMap.put("URL", "");
+//                    resultMap.put("FAILCNT", failCnt+1);
+//                    idCheck.LoginUpdate(failCnt+1, "N", id, dateTime);
+//                    mngRepository.save(idCheck);
+//                }
+//            }else {
+//                // 로그인 실패
+//                resultMap.put("RESULT", "LOGIN_FAIL");
+//                resultMap.put("URL", "");
+//            }
         }
         HttpSession session = request.getSession(true);
         session.removeAttribute("userId");
